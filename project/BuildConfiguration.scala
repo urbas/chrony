e8f3836a85a50d2b5cd4e0a065c5cdf140467278
Matej Urbas <matej.urbas@gmail.com>
2014-08-08 09:22:19 +0100

    Bumped the version of Scala.

--- project/BuildConfiguration.scala
+++ project/BuildConfiguration.scala
@@ -1,9 +1,11 @@
-import sbt._
 import sbt.Keys._
+import sbt._
 
 object BuildConfiguration extends Build {
+
   override def settings: Seq[Def.Setting[_]] = super.settings ++ Seq(
-    organization := "si.urbas"
+    organization := "si.urbas",
+    scalaVersion := "2.11.1"
   )
 
   lazy val root = Project(
@@ -14,6 +16,13 @@ object BuildConfiguration extends Build {
 
   lazy val chrony = Project(
     id = "chrony",
-    base = file("chrony")
+    base = file("chrony"),
+    settings = Seq(
+      libraryDependencies ++= Seq(
+        "com.novocode" % "junit-interface" % "0.10" % "test",
+        "junit" % "junit" % "4.11" % "test"
+      )
+    )
   )
+
 }
