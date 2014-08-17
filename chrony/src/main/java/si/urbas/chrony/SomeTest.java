4c32cd82cdcc9c498c8bcdf2362f4ad11a4e30b9
Matej Urbas <matej.urbas@gmail.com>
2014-08-17 16:17:50 +0100

    Created the gradle-based android Chrony projects.

---
+++ chrony/src/main/java/si/urbas/chrony/SomeTest.java
@@ -0,0 +1,9 @@
+package si.urbas.chrony;
+
+/**
+ * Created by matej on 17/08/14.
+ */
+public class SomeTest {
+
+    public static final String SOME_CONSTANT = "this be a very unique text! Nya!";
+}
---
+++ chronyApp/src/main/java/si/urbas/chrony/app/DataEntry.java
@@ -0,0 +1,38 @@
+package si.urbas.chrony.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import si.urbas.chrony.SomeTest;
+
+
+public class DataEntry extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_data_entry);
+    TextView textView = (TextView) findViewById(R.id.textView);
+    textView.setText(SomeTest.SOME_CONSTANT);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.data_entry, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    return id == R.id.action_settings || super.onOptionsItemSelected(item);
+  }
+}
