110669598349fb3102d1c9b82f341622310b884a
Matej Urbas <matej.urbas@gmail.com>
2014-08-30 11:31:46 +0100

    Upgrader: renamed.

--- chronyLib/src/main/java/si/urbas/chrony/util/Upgrader.java
+++ chronyLib/src/main/java/si/urbas/chrony/util/Upgrader.java
@@ -1,15 +1,28 @@
 package si.urbas.chrony.util;
 
+import java.util.ArrayList;
+
 public class Upgrader<T> {
 
-  private final UnidirectionalUpgrader<T> unidirectionalUpgrader = new UnidirectionalUpgrader<T>();
+  private final ArrayList<UpgradeScript<T>> upgradeScripts = new ArrayList<UpgradeScript<T>>();
 
   public Upgrader<T> add(UpgradeScript<T> upgradeScript) {
-    unidirectionalUpgrader.add(upgradeScript);
+    upgradeScripts.add(upgradeScript);
     return this;
   }
 
   public void upgrade(T upgradeData, int currentVersion, int newVersion) {
-    unidirectionalUpgrader.upgrade(upgradeData, currentVersion, newVersion);
+    if (newVersion > upgradeScripts.size()) {
+      throw new IllegalArgumentException("Could not upgrade to version " + newVersion + ". The version is too high.");
+    }
+    if (currentVersion < 0) {
+      throw new IllegalArgumentException("Current version must be non-negative.");
+    }
+    if (currentVersion > newVersion) {
+      throw new IllegalArgumentException("The new version is greater than the current version. Cannot downgrade.");
+    }
+    for (int i = currentVersion; i < newVersion; i++) {
+      upgradeScripts.get(i).upgrade(upgradeData);
+    }
   }
 }
--- chronyLib/src/test/java/si/urbas/chrony/util/UpgraderTest.java
+++ chronyLib/src/test/java/si/urbas/chrony/util/UpgraderTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
-public class UnidirectionalUpgraderTest {
+public class UpgraderTest {
 
   private static final int VERSION_0 = 0;
   private static final int VERSION_1 = 1;
   private static final int VERSION_2 = 2;
   private static final int VERSION_4 = 4;
   private static final String UPGRADE_DATA = "upgrade data";
-  private UnidirectionalUpgrader<String> unidirectionalUpgrader;
+  private Upgrader<String> upgrader;
   private UpgradeScript<String> version1;
   private UpgradeScript<String> version2;
   private UpgradeScript<String> version3;
@@ -23,7 +23,7 @@ public class UnidirectionalUpgraderTest {
     version1 = mock(UpgradeScript.class);
     version2 = mock(UpgradeScript.class);
     version3 = mock(UpgradeScript.class);
-    unidirectionalUpgrader = new UnidirectionalUpgrader<String>()
+    upgrader = new Upgrader<String>()
       .add(version1)
       .add(version2)
       .add(version3);
@@ -31,41 +31,41 @@ public class UnidirectionalUpgraderTest {
 
   @Test
   public void upgrade_MUST_call_the_upgrade_script_for_the_first_version() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, VERSION_0, VERSION_1);
+    upgrader.upgrade(UPGRADE_DATA, VERSION_0, VERSION_1);
     verify(version1).upgrade(UPGRADE_DATA);
   }
 
   @Test
   public void upgrade_MUST_call_the_upgrade_script_for_the_second_version() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, VERSION_1, VERSION_2);
+    upgrader.upgrade(UPGRADE_DATA, VERSION_1, VERSION_2);
     verify(version2).upgrade(UPGRADE_DATA);
   }
 
   @Test
   public void upgrade_MUST_call_only_the_second_upgrade_script_WHEN_upgrading_from_first_to_second_version() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, VERSION_1, VERSION_2);
+    upgrader.upgrade(UPGRADE_DATA, VERSION_1, VERSION_2);
     verifyZeroInteractions(version1);
     verifyZeroInteractions(version3);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void upgrade_MUST_throw_an_exception_WHEN_an_upgrade_script_for_the_given_version_does_not_exist() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, VERSION_1, VERSION_4);
+    upgrader.upgrade(UPGRADE_DATA, VERSION_1, VERSION_4);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void upgrade_MUST_throw_an_exception_WHEN_the_current_version_is_negative() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, -VERSION_1, VERSION_2);
+    upgrader.upgrade(UPGRADE_DATA, -VERSION_1, VERSION_2);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void upgrade_MUST_throw_an_exception_WHEN_the_current_version_is_larger_than_the_new_version() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, VERSION_2, VERSION_1);
+    upgrader.upgrade(UPGRADE_DATA, VERSION_2, VERSION_1);
   }
 
   @Test
   public void upgrade_MUST_not_invoke_any_scripts_WHEN_the_current_version_the_same_as_the_new_version() {
-    unidirectionalUpgrader.upgrade(UPGRADE_DATA, VERSION_2, VERSION_2);
+    upgrader.upgrade(UPGRADE_DATA, VERSION_2, VERSION_2);
     verifyZeroInteractions(version1);
     verifyZeroInteractions(version2);
     verifyZeroInteractions(version3);
